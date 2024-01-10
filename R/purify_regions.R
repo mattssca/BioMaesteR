c80a4bf34b8a3efd2fee1d1234feb99a46cdb409
mattssca <mattsada@gmail.com>
2024-01-10 14:42:21 -0800

    Updates to purify_regions to take chromosomeal ranges into consideration.

--- R/purify_regions.R
+++ R/purify_regions.R
@@ -11,7 +11,10 @@
 #' `qchrom` (string), `qstart` (string, or integer), and `qend` (string or integer).
 #' These parameters can also accept a vector of characters for multiple regions.
 #' The function also handles chromosome prefixes in the returned object,
-#' based on the selected `projection`.
+#' based on the selected `projection`. In addition, this function also checks if
+#' the provided start coordinate is equal or greater to the end coordinate for 
+#' the same chromosome. It also ensures that specified ranges are within the 
+#' actual chromosomal range.
 #'
 #' @param these_regions The region(s) to be queried. Can be a data frame with
 #' regions with the following columns; chrom, start, end.
@@ -61,9 +64,6 @@ purify_regions <- function(these_regions = NULL,
          `these_regions` or a combination of `qchrom`, `qstart`, and `qend`")
   }
 
-  #TODO: Checks if the specified region is outside the actual chromosomal ranges.
-  #TODO: Check if end is greater than start.
-
   #wrangle the regions provided
   if(!is.null(these_regions)){
     if(is.data.frame(these_regions)){
@@ -102,10 +102,45 @@ purify_regions <- function(these_regions = NULL,
          or individually specify the chromosome, start and end positions with;
          `qchrom`, `qstart`, and `qend`")
   }
-
+  
+  #enforce data types
+  region_table = region_table %>%
+    dplyr::mutate(chrom = as.character(chrom),
+                  start = as.integer(start),
+                  end = as.integer(end))
+  
   #run helper function to deal with prefixes
   region_table = purify_chr(projection = projection,
                             incoming_table = region_table)
+  
+  #check if regions make sense 
+  #is start equal or greater then end?
+  for (i in 1:nrow(region_table)){
+    if (region_table$start[i] >= region_table$end[i]) {
+      print(paste("Row", i, "does not meet the condition: start is greater than or equal to end"))
+    }
+  }
+  
+  #check if regions are within the chromosomal range
+  commpare_ranges <- function(df1, df2){
+    result <- df1 %>%
+      inner_join(df2, by = "chrom") %>%
+      filter(start.x > end.y | end.x > end.y)
+    
+    if(nrow(result) > 0){
+      stop("Specified start or end coordinates fall outside the actual chromosomal range")
+    }
+  }
+  
+  if(projection == "hg38"){
+    commpare_ranges(region_table, chromosome_arms_hg38 %>% 
+                      dplyr::filter(arm == "q"))
+  }else if(projection == "grch37"){
+    compare_ranges(region_table, chromosome_arms_grch37 %>% 
+                     dplyr::filter(arm == "q"))
+  }else{
+    stop("The projection you have provided is not supported. Please choose either hg38 or grch37")
+  }
 
   #enforce data types
   region_table$chrom = as.character(region_table$chrom)
